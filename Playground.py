224b0cf010acb531c121eaed73fe254f215a9188
Michael Householder <householdcodermike@gmail.com>
2019-08-27 17:11:48 -0400

    Add files via upload

---
+++ Playground.py
@@ -0,0 +1,33 @@
+import turtle
+bob = turtle.Turtle()
+
+import turtle
+
+steve = turtle.Turtle()
+
+
+
+print(bob.lt(90))
+print(bob.fd(200))
+print(bob.lt(180))
+print(bob.fd(100))
+print(bob.lt(90))
+print(bob.fd(100))
+print(bob.lt(90))
+print(bob.fd(100))
+print(bob.rt(180))
+print(bob.fd(200))
+
+
+steve.penup()
+steve.forward(200)
+steve.left(90)
+steve.pendown()
+steve.forward(150)
+steve.penup()
+steve.forward(25)
+steve.pendown()
+steve.forward(25)
+
+
+
